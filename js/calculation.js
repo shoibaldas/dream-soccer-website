5bbb9413a2f5cdae2c8c8b9f68bd5d475284d3e6
shoibaldas <shoibalshantu@gmail.com>
2022-08-23 05:29:11 +0600

    Validation added

--- js/calculation.js
+++ js/calculation.js
@@ -1,19 +1,59 @@
 document.getElementById('btn-calculate-player-cost').addEventListener('click', function () {
-    const currentPlayerExpense = getInputFieldValueById('per-player-cost');
-    const playerList = checkPlayerListById('player-list');
 
-    const newPlayerrExpenses = currentPlayerExpense * playerList;
-
-    setTextElementValueById('player-expenses-total', newPlayerrExpenses);
+    const checkList = document.getElementById('player-list').innerText;
+    const checkPlayerInputField = document.getElementById('per-player-cost').value;
+    if (checkList == "") {
+        alert("Select Player First!");
+    }
+    else if (checkPlayerInputField == "") {
+        document.getElementById("per-player-cost").style.borderColor = "red";
+    }
+    else if (isNaN(checkPlayerInputField)) {
+        alert('Enter money amount for player in number!');
+    }
+    else {
+        document.getElementById("per-player-cost").style.borderColor = "green";
+        const currentPlayerExpense = getInputFieldValueById('per-player-cost');
+        const playerList = checkPlayerListById('player-list');
+        const newPlayerrExpenses = currentPlayerExpense * playerList;
+        setTextElementValueById('player-expenses-total', newPlayerrExpenses);
+    }
 })
 
 document.getElementById('btn-calculate-total').addEventListener('click', function () {
 
-    const playerExpenseTotal = getTextElementValueById('player-expenses-total');
-    const managerCost = getInputFieldValueById('manager-cost');
-    const coachCost = getInputFieldValueById('coach-cost');
+    const checkList = document.getElementById('player-list').innerText;
+    const checkPlayerInputField = document.getElementById('per-player-cost').value;
+    const checkManagerInputField = document.getElementById('manager-cost').value;
+    const checkCoachInputField = document.getElementById('coach-cost').value;
 
-    const costTotal = playerExpenseTotal + managerCost + coachCost;
+    if (checkList == "") {
+        alert("Select Player First!");
+    }
+    else if (checkPlayerInputField == "") {
+        document.getElementById("per-player-cost").style.borderColor = "red";
+    }
+    else if (checkManagerInputField == "") {
+        document.getElementById("manager-cost").style.borderColor = "red";
+    }
+    else if (checkCoachInputField == "") {
+        document.getElementById("coach-cost").style.borderColor = "red";
+    }
+    else if (isNaN(checkManagerInputField)) {
+        alert('Enter money amount for manager in number!');
+    }
+    else if (isNaN(checkCoachInputField)) {
+        alert('Enter money amount for coach in number!');
+    }
+    else {
+        document.getElementById("per-player-cost").style.borderColor = "green";
+        document.getElementById("manager-cost").style.borderColor = "green";
+        document.getElementById("coach-cost").style.borderColor = "green";
 
-    setTextElementValueById('cost-total', costTotal);
+        const playerExpenseTotal = getTextElementValueById('player-expenses-total');
+        const managerCost = getInputFieldValueById('manager-cost');
+        const coachCost = getInputFieldValueById('coach-cost');
+        const costTotal = playerExpenseTotal + managerCost + coachCost;
+        setTextElementValueById('cost-total', costTotal);
+    }
 })
